be7a3e7d9676d00e93e9e92fcd3b1c9df3c17b98
pa-k-kakihara <kota.kakihara@phoneappli.net>
2021-04-04 00:57:10 +0900

    video-chat機能実装①

--- nuxt-skyway/pages/index.vue
+++ nuxt-skyway/pages/index.vue
@@ -1,38 +1,134 @@
 <template>
-  <div class="container">
+  <section class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-video-chat
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <logo/>
+      <video id="their-video" width="200" autoplay playsinline></video>
+      <video id="my-video" muted="true" width="200" autoplay playsinline></video>
+
+      <div class="main">
+        <h2>Nuxt.js + SkyWayのビデオチャット</h2>
+        マイク:
+        <select v-model="selectedAudio" @change="onChange">
+          <option disabled value="">Please select one</option>
+          <option v-for="(audio, key, index) in audios" v-bind:key="index" :value="audio.value">
+            {{ audio.text }}
+          </option>
+        </select>
+
+        カメラ: 
+        <select v-model="selectedVideo" @change="onChange">
+          <option disabled value="">Please select one</option>
+          <option v-for="(video, key, index) in videos" v-bind:key="index" :value="video.value">
+            {{ video.text }}
+          </option>
+        </select>
+
+        <div>
+          <p>Your id: <span id="my-id">{{peerId}}</span></p>
+          <p>他のブラウザでこのIDをコールしましょう。</p>
+          <h3>コールする</h3>
+          <input v-model="calltoid" placeholder="call id">
+          <button @click="makeCall" class="button--green">Call</button>
+        </div>
       </div>
+
     </div>
-  </div>
+  </section>
 </template>
 
-<script lang="ts">
+<script>
 import Logo from '~/components/Logo.vue'
 import Peer from 'skyway-js';
-import Vue from 'vue'
 
-export default Vue.extend({})
+export default {
+  components: {
+    Logo
+  },
+
+  data () {
+    return {
+      APIKey: '2b225ae2-6630-4e12-9536-acb3d3587446',
+      selectedAudio: '',
+      selectedVideo: '',
+      audios: [],
+      videos: [],
+      localStream: null,
+      peerId: '',
+      calltoid: ''
+    }
+  },
+
+  methods: {
+    onChange: function () {
+      if(this.selectedAudio != '' && this.selectedVideo != ''){
+        this.connectLocalCamera();
+      }
+    },
+
+    connectLocalCamera: async function(){
+      const constraints = {
+        audio: this.selectedAudio ? { deviceId: { exact: this.selectedAudio } } : false,
+        video: this.selectedVideo ? { deviceId: { exact: this.selectedVideo } } : false
+      }
+
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      document.getElementById('my-video').srcObject = stream;
+      this.localStream = stream;
+    },
+
+    makeCall: function () {
+      console.log('makeCall');
+      const call = this.peer.call(this.calltoid, this.localStream);
+      this.connect(call);
+    },
+
+    connect: function (call) {
+      call.on('stream', stream => {
+        const el = document.getElementById('their-video');
+        el.srcObject = stream;
+        el.play();
+      });
+    }
+  },
+
+  mounted: function () {
+    this.peer = new Peer({
+      key:   this.APIKey,
+      debug: 3,
+    });
+
+    this.peer.on('open', () => {
+      this.peerId = this.peer.id
+    });
+
+    this.peer.on('call', call => {
+      call.answer(this.localStream);
+      this.connect(call);
+    });
+
+    //デバイスへのアクセス
+    navigator.mediaDevices.enumerateDevices()
+    .then((deviceInfos) => {
+      for (let i = 0; i !== deviceInfos.length; ++i) {
+        const deviceInfo = deviceInfos[i]
+        if (deviceInfo.kind === 'audioinput') {
+          this.audios.push({
+            text: deviceInfo.label ||
+            `Microphone ${this.audios.length + 1}`,
+            value: deviceInfo.deviceId
+          })
+        } else if (deviceInfo.kind === 'videoinput') {
+          this.videos.push({
+            text: deviceInfo.label ||
+            `Camera  ${this.videos.length - 1}`,
+            value: deviceInfo.deviceId
+          })
+        }
+      }
+    })
+
+  }
+}
 </script>
 
 <style>
